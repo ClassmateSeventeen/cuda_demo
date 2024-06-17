310c6d104827f22fee0f8fae0988759e2f69be0e
ClassmateSeventeen <44745736+ClassmateSeventeen@users.noreply.github.com>
2024-06-17 19:20:00 +0800

    Update sum.py

--- pytorch/ops/ops_py/sum.py
+++ pytorch/ops/ops_py/sum.py
@@ -1,1 +1,27 @@
+import torch
+from torch.autograd import Function
+import sum_single
+import sum_double
 
+class SumSingle(Function):
+
+    @staticmethod
+    def forward(ctx, array):
+      #pass
+
+    @staticmethod
+    def backward(ctx, g_out):
+      #pass
+
+class SumDouble(Function):
+
+    @staticmethod
+    def forward(ctx, array):
+      #pass
+
+    @staticmethod
+    def backward(ctx, g_out):
+      #pass
+
+sum_single_op = SumSingle.apply
+sum_double_op = SumDouble.apply
